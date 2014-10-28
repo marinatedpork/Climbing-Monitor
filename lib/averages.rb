1cb040f3c01c1e8c253387f8256bef3b6608d3db
Matthew Meyers <m.j.mey3rs@gmail.com>
2014-10-28 09:05:21 -0500

    Hey

--- lib/averages.rb
+++ lib/averages.rb
@@ -1,3 +1,4 @@
+require 'csv'
 thing = CSV.open('../db/all_routes.csv').read
 
 sport = thing.select { |x| x[1] == 'sport'}
@@ -7,13 +8,23 @@ alpine = thing.select { |x| x[1] == 'alpine'}
 boulder = thing.select { |x| x[1] == 'boulder'}
 mixed = thing.select { |x| x[1] == 'mixed'}
 
-sport_numbers = sport.select { |x| x[3] }
-trad_numbers = trad.select { |x| x[3]}
-ice_numbers = ice.select { |x| x[3]}
-alpine_numbers = alpine.select { |x| x[3]}
-boulder_numbers = boulder.select { |x| x[3]}
-mixed_numbers = mixed.select { |x| x[3]}
+sport_numbers = []
+trad_numbers = []
+ice_numbers = []
+alpine_numbers = []
+boulder_numbers = []
+mixed_numbers = []
 
+sport.each { |x| sport_numbers << x[3] }
+trad.each { |x| trad_numbers << x[3] }
+ice.each { |x| ice_numbers << x[3] }
+alpine.each { |x| alpine_numbers << x[3] }
+boulder.each { |x| boulder_numbers << x[3] }
+mixed.each { |x| mixed_numbers << x[3] }
 
-
-
+puts sport_numbers.map(&:to_i).inject(:+) / sport_numbers.length
+puts trad_numbers.map(&:to_i).inject(:+) / trad_numbers.length
+puts ice_numbers.map(&:to_i).inject(:+) / ice_numbers.length
+puts alpine_numbers.map(&:to_i).inject(:+) / alpine_numbers.length
+puts mixed_numbers.map(&:to_i).inject(:+) / mixed_numbers.length
+puts boulder_numbers.map(&:to_i).inject(:+) / boulder_numbers.length
